7ff34d59d385962f65d80e63209c23cc642640da
Marwan Attar <marwanattar@Marwans-MacBook-Pro.local>
2023-08-02 11:14:55 +1000

    Parametrising the variables

--- imperative/script.sh
+++ imperative/script.sh
@@ -1,18 +1,13 @@
 #!/bin/bash
 
-#source ~/params.sh
-#echo $USERNAME
-#echo $PASSWORD
-#echo $APIC_SERVER
-#echo $ORG
-#echo $CATALOG
+source ~/params.sh
 echo "Logging in to the server....."
 
-apic-slim login --username leovidal --password meu1flamengo --server cpd-cp4i.nz-police-poc-e02a6b44bc9165598fc99ac3b2541da6-0000.au-syd.containers.appdomain.cloud/integration/apis/cp4i/medium --realm provider/default-idp-2
+apic-slim login --username $USERNAME --password $PASSWORD --server $APIC_SERVER --realm provider/default-idp-2
 
 echo "Publishing product...."
 
-apic-slim products:publish --server cpd-cp4i.nz-police-poc-e02a6b44bc9165598fc99ac3b2541da6-0000.au-syd.containers.appdomain.cloud/integration/apis/cp4i/medium  --org nz-police --catalog police product.yaml
+apic-slim products:publish --server $APIC_SERVER --org $ORG --catalog $POLICE product.yaml
 
 echo "All done"
 
